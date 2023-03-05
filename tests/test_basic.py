f2a75036fe0f54b52634059a747496ac1b979f61
capitanFlint <tekkengod@list.ru>
2023-03-05 12:16:52 +0300

    check tests CI

---
+++ tests/test_basic.py
@@ -0,0 +1,14 @@
+def setup_module(module):
+    print("basic setup module")
+
+
+def teardown_module(module):
+    print("basic teardown module")
+
+
+def test_1():
+    assert 1 + 1 == 2
+
+
+def test_2():
+    assert "1" + "1" == "11"
