6c19c0ee945256bbee4936b4675b091133aceeda
ixorlive <igorshakirov43@gmail.com>
2023-03-06 08:16:07 +0300

    add Assign

--- command/command_factory.py
+++ command/command_factory.py
@@ -2,7 +2,7 @@ from command import commands
 
 
 class CommandFactory:
-    def create_command_base(self, command_name: str) -> commands.CommandBase:
+    def create_command_base(self, command_name: str, **args) -> commands.CommandBase:
         if command_name == "cat":
             return commands.Cat()
         elif command_name == "echo":
@@ -11,4 +11,6 @@ class CommandFactory:
             return commands.Wc()
         elif command_name == "pwd":
             return commands.Pwd()
+        elif command_name == "=":
+            return commands.Assign(var_name=args["var_name"], var_value=args["var_value"])
         return commands.External(command_name)
