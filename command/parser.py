bb7778849c9b6da9328511c6f9fcbad4f277afe7
ixorlive <igorshakirov43@gmail.com>
2023-03-06 08:11:29 +0300

    add parser + tests

--- command/parser.py
+++ command/parser.py
@@ -1,11 +1,45 @@
-from command.commands import Command
-from command.lexer import Lexer
+from typing import List
+
+from command.commands import CommandBase, Command
+from command.lexer import Lexer, Token
+from command.command_factory import CommandFactory
+
+
+class ParsingError(Exception):
+    pass
 
 
 class Parser:
-    def __init__(self, command_factory, lexer: Lexer):
-        pass
+    def __init__(self, command_factory: CommandFactory, lexer: Lexer):
+        self.lexer = lexer
+        self.command_factory = command_factory
 
-    # TODO может parse_command
-    def parse_program(self, command_line: str) -> list[Command]:
-        pass
+    def parse_program(self) -> List[Command]:
+        commands = []
+        args = []
+        command = None
+        for token in self.lexer:
+            if token.type == "OPERATOR":
+                if token.value == "|":
+                    commands.append(Command(command, args))
+                    command = None
+                    args = []
+                else:
+                    raise ParsingError("Unknown operator " + token.value)
+            elif command is None:
+                command = self._parse_command(token.value)
+            else:
+                args.append(token.value)
+        if command is not None:
+            commands.append(Command(command, args))
+        return commands
+
+    def _parse_command(self, command_str: str) -> CommandBase:
+        if '=' in command_str:
+            variable = command_str.split('=')
+            return self.command_factory.create_command_base(
+                command_name="=",
+                var_name=variable[0],
+                var_value=variable[1]
+            )
+        return self.command_factory.create_command_base(command_str)
---
+++ tests/test_parser.py
@@ -0,0 +1,54 @@
+from typing import Tuple
+import pytest
+
+from command.parser import Parser, ParsingError
+from command.lexer import Lexer
+from command.commands import *
+from command.command_factory import CommandFactory
+
+
+@pytest.mark.parametrize("input_string, expected_commands", [
+    ("", []),
+    ("echo \"Hello World\"", [
+        (Echo, ["Hello World"])
+    ]),
+    ("echo 123 | wc", [
+        (Echo, ["123"]),
+        (Wc, [])
+    ]),
+    ("echo text.txt | cat -n", [
+        (Echo, ["text.txt"]),
+        (Cat, ["-n"])
+    ]),
+    ("file=text", [
+        (Assign, [])
+    ]),
+    ("cat test | echo test | wc test test test | cat test", [
+        (Cat, ["test"]),
+        (Echo, ["test"]),
+        (Wc, ["test", "test", "test"]),
+        (Cat, ["test"])
+    ]),
+    ("pwd test 'test test test' | echo", [
+        (Pwd, ["test", "test test test"]),
+        (Echo, [])
+    ])
+])
+def test_parser(input_string: str, expected_commands: List[Tuple[Command, List[str]]]):
+    lexer = Lexer(input_string)
+    commands = Parser(CommandFactory(), lexer).parse_program()
+    assert len(commands) == len(expected_commands)
+    for actual_command, expected_command in zip(commands, expected_commands):
+        assert isinstance(actual_command._base, expected_command[0])
+        assert actual_command._args == expected_command[1]
+
+
+@pytest.mark.parametrize("input_string", [
+    "echo 1+1 | cat hello - hello",
+    "echo 123 < text.txt",
+    "pwd >> echo 123"
+])
+def test_parser_exception(input_string: str):
+    with pytest.raises(ParsingError):
+        lexer = Lexer(input_string)
+        commands = Parser(CommandFactory(), lexer).parse_program()
