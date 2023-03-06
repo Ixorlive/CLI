188a5689c8325ab4bbda97d8a6b78f9eb0b9346d
ixorlive <igorshakirov43@gmail.com>
2023-03-06 07:07:43 +0300

    add lexer + tests

--- command/lexer.py
+++ command/lexer.py
@@ -1,3 +1,60 @@
-class Lexer:
-    # TODO Возможно здесь есть что-то полезное https://docs.python.org/3/library/shlex.html
-    pass
+import shlex
+from typing import Iterable, List, NamedTuple
+import operator
+
+
+class Token(NamedTuple):
+    type: str
+    value: str
+
+
+class Lexer(Iterable):
+
+    def __init__(self, input_line: str):
+        self.tokens = self.tokenize(input_line)
+        self.index = 0
+
+    def tokenize(self, input_line: str) -> List[Token]:
+        lexer = shlex.shlex(input_line, posix=True)
+        lexer.whitespace_split = True
+        tokens = []
+        for token in lexer:
+            if token.isdigit():
+                tokens.append(Token(
+                    type='INTEGER',
+                    value=token)
+                )
+            elif self._is_operator(token):
+                tokens.append(Token(
+                    type='OPERATOR',
+                    value=token)
+                )
+            else:
+                tokens.append(Token(
+                    type='IDENTIFIER',
+                    value=token)
+                )
+        return tokens
+
+    def __iter__(self) -> 'Lexer':
+        self.index = 0
+        return self
+
+    def __next__(self) -> Token:
+        if self.index >= len(self.tokens):
+            raise StopIteration
+        self.index += 1
+        return self.tokens[self.index - 1]
+
+    def current_token(self) -> Token:
+        return self.tokens[self.index]
+
+    def _is_operator(self, token):
+        shell_operators = ['|', '<', '>', '&&', '||']
+        if token in shell_operators:
+            return True
+        try:
+            op_func = getattr(operator, token)
+            return callable(op_func)
+        except AttributeError:
+            return False
