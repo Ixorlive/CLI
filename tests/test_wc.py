0dba43a765051d822a3d753e8086cb7c65020915
ixorlive <igorshakirov43@gmail.com>
2023-03-08 04:01:06 +0300

    fix wc test for wsl ubuntu and windows

--- tests/test_wc.py
+++ tests/test_wc.py
@@ -1,5 +1,6 @@
 from io import StringIO
 import pytest
+import os
 from command.wc import Wc, TextResult, FileResult
 
 
@@ -15,19 +16,19 @@ def test_wc_base(input_text: str, expected_result: TextResult):
 
 
 @pytest.mark.parametrize("input_files, expected_result", [
-    (["data/empty.txt"],
-       [FileResult("data/empty.txt", TextResult(0, 0, 0))]
-    ),
-    (["data/text1.txt"],
-        [FileResult("data/text1.txt", TextResult(18, 641, 4056))]
-    ),
-    (["data/big_text.txt"],
-        [FileResult("data/big_text.txt", TextResult(51, 320, 1705))]
-    ),
-    (["data/empty.txt", "data/text1.txt", "data/big_text.txt"],
-        [FileResult("data/empty.txt", TextResult(0, 0, 0)),
-         FileResult("data/text1.txt", TextResult(18, 641, 4056)),
-         FileResult("data/big_text.txt", TextResult(51, 320, 1705))]
+    (["tests/data/empty.txt"],
+     [FileResult("tests/data/empty.txt", TextResult(0, 0, 0))]
+     ),
+    (["tests/data/text1.txt"],
+     [FileResult("tests/data/text1.txt", TextResult(18, 641, 4056))]
+     ),
+    (["tests/data/big_text.txt"],
+     [FileResult("tests/data/big_text.txt", TextResult(12, 379, 1705))]
+     ),
+    (["tests/data/empty.txt", "tests/data/text1.txt", "tests/data/big_text.txt"],
+     [FileResult("tests/data/empty.txt", TextResult(0, 0, 0)),
+      FileResult("tests/data/text1.txt", TextResult(18, 641, 4056)),
+      FileResult("tests/data/big_text.txt", TextResult(12, 379, 1705))]
      )
 ])
 def test_wc_files(input_files, expected_result):
@@ -41,3 +42,26 @@ def test_wc_files(input_files, expected_result):
         assert actual.text_result.count_words == expected.text_result.count_words
         assert actual.text_result.count_lines == expected.text_result.count_lines
         assert actual.filename == expected.filename
+
+
+@pytest.mark.parametrize("args, input_text, expected_output", [
+    ([], "hello world\nhow are you?", "      1       5      24\n"),
+    ([], "", "      0       0       0\n"),
+    ([], "one\nline\nfile\n", "      3       3      14\n"),
+    (["tests/data/big_text.txt"], "", "  12  379 2052 tests/data/big_text.txt\n"),
+    (["not_found.txt"], "", "wc: not_found.txt: No such file or directory\n"),
+    (["tests/data/text1.txt", "tests/data/cat_data.txt"], "", "  18  641 4038 tests/data/text1.txt\n"
+                                                              "   2    2    8 tests/data/cat_data.txt\n"
+                                                              "  20  643 4046 total\n"),
+    (["tests/data/big_text.txt", "not_found"], "", "  12  379 2052 tests/data/big_text.txt\n"
+                                                   "wc: not_found: No such file or directory\n"
+                                                   "  12  379 2052 total\n")
+])
+def test_wc_execute_text(args, input_text, expected_output):
+    """
+
+    """
+    output_stream = StringIO()
+    Wc().execute(args, input_stream=StringIO(input_text), output_stream=output_stream, error_stream=None)
+    assert output_stream.getvalue() == expected_output
+    # assert true
