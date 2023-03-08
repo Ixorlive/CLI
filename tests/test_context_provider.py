ff8b25c49353923826204e8ad2fe40b06aae2b99
ixorlive <igorshakirov43@gmail.com>
2023-03-08 03:11:15 +0300

    add test if variable is not found

--- tests/test_context_provider.py
+++ tests/test_context_provider.py
@@ -8,3 +8,10 @@ def test_set_and_get_variables():
     var_value = "BBBB"
     context_provider.set_variable(var_name, var_value)
     assert context_provider.get_variable(var_name) == var_value
+
+
+def test_no_found_variables():
+    context_provider = ContextProvider()
+    assert context_provider.get_variable("test") is None
+    context_provider.set_variable("test", "asd")
+    assert context_provider.get_variable("test") is not None
