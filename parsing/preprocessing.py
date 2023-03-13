84cf20d05f5174b6e5167d02e385dd615b6ac3e6
ixorlive <igorshakirov43@gmail.com>
2023-03-13 09:07:10 +0300

    add comments

--- parsing/preprocessing.py
+++ parsing/preprocessing.py
@@ -1,8 +1,11 @@
 from environment.context_provider import ContextProvider
-from typing import Tuple
 
 
 class Preprocessor:
+    """
+    The Preprocessor class takes a ContextProvider object as input and provides methods for preprocessing input strings,
+    replacing variables with their values.
+    """
 
     def __init__(self, context_provider: ContextProvider):
         self.context = context_provider
@@ -10,36 +13,55 @@ class Preprocessor:
         self._in_double_quotes = False
 
     def preprocess(self, input_str: str) -> str:
+        """
+
+        Replaces occurrences of variables in the input string with their values, based on the provided ContextProvider.
+        Variables are denoted by the '$' symbol.
+        If the variable is inside single quotes, no substitution is done.
+        If there is a '\' character before the '$' symbol, it is skipped and the '$' symbol is added to the output.
+        If the variable is inside a substitution (${var_name}) - no supported case =( :TODO add support
+        If there are no valid characters for the variable name after the '$' symbol, no substitution is done.
+        If the ContextProvider returns None for a variable, it is replaced with an empty string.
+        If the variable name starts with a number, the '$' symbol
+            and the number are replaced with an empty string (as in bash).
+        In all other cases, the variable is substituted with its value, e.g. '$var_name' -> 'value'.
+        """
         result = ""
         while 1:
             pos_variable = self._find_pos(input_str)
-            if pos_variable > 0 and input_str[pos_variable - 1] == '\\':
-                result += input_str[:pos_variable - 1] + "$"
-                input_str = input_str[pos_variable + 1:]
+            if pos_variable > 0 and input_str[pos_variable - 1] == "\\":
+                result += input_str[: pos_variable - 1] + "$"
+                input_str = input_str[pos_variable + 1 :]
             elif pos_variable != -1:
                 result += input_str[:pos_variable]
-                var_name = self._get_variable_name(input_str[pos_variable + 1:])
+                var_name = self._get_variable_name(input_str[pos_variable + 1 :])
                 if not var_name:
                     result += "$"
                 else:
                     value = self.context.get_variable(var_name)
                     result += value if value else ""
-                input_str = input_str[pos_variable + len(var_name) + 1:]
+                input_str = input_str[pos_variable + len(var_name) + 1 :]
             else:
                 result += input_str
                 break
         return result
 
     def _find_pos(self, src_str: str) -> int:
+        """
+        Finds the position '$' character that is not inside a single-quoted
+        """
         for pos, c in enumerate(src_str):
-            self._in_single_quotes ^= (c == '\'' and not self._in_double_quotes)
-            self._in_double_quotes ^= (c == '\"' and not self._in_single_quotes)
-            if not self._in_single_quotes and c == '$':
+            self._in_single_quotes ^= c == "'" and not self._in_double_quotes
+            self._in_double_quotes ^= c == '"' and not self._in_single_quotes
+            if not self._in_single_quotes and c == "$":
                 return pos
         return -1
 
     @staticmethod
     def _get_variable_name(src_str: str) -> str:
+        """
+        Gets the variable name following a '$' character in a string.
+        """
         variable_name = ""
         if src_str[0].isnumeric():
             return src_str[0]
@@ -52,14 +74,4 @@ class Preprocessor:
 
     @staticmethod
     def _is_valid(c: str) -> bool:
-        return c.isalpha() or c.isnumeric() or c == '_'
-
-
-if __name__ == "__main__":
-    context: ContextProvider = ContextProvider()
-    context.set_variable("test", "hello")
-    context.set_variable("test_t", "hello1")
-    context.set_variable("test12_", "hello2")
-    input_line = "echo \"$test's $test12_\""
-    preprocessor = Preprocessor(context)
-    print(preprocessor.preprocess(input_line))
+        return c.isalpha() or c.isnumeric() or c == "_"
